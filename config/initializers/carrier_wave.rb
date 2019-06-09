cd1acb3d7e6955217c7c6f6124c5e06d7be001dd
Brandon Caiza <geniusrobotizo@gmail.com>
2019-06-09 15:38:57 -0700

    Proximy V1.0.1

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-	Carrierwave.configure do |config|
+	CarrierWave.configure do |config|
 		config.fog_credentials = {
 			provider: 'AWS', 
 			aws_access_key_id: ENV['S3_ACCESS_KEY'],
