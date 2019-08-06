5fac793355e6ded88af9e1313eef21a3aef988fa
Brandon Caiza <geniusrobotizo@gmail.com>
2019-08-06 16:49:46 -0700

    3

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,8 @@
 class MainController < ApplicationController
 	skip_before_action :authorize
 	  def index
-	  	@user = current_user
 	  	if session[:user_id] 
+	  		@user = current_user
 	  		@users = User.all.sort_by {|user| user.userInterests(current_user) + user.ccLocation(current_user.latitude, current_user.longitude) }.reverse
 	  	end
 	  	posts = Post.all
