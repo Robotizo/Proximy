93e072500eedf1613b8bdc6afed034f8f70ed2ef
geniusrobotizo@gmail.com <geniusrobotizo@gmail.com>
2019-08-23 19:55:29 -0700

    Adds:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:session][:email].downcase)
  
 		if user && user.authenticate(params[:session][:password])
-			if user.email_confirmed == true
+			#if user.email_confirmed == true
 			    session[:user_id] = user.id
 				user.login_increment
 				if user.sign_in_count == 1
@@ -18,10 +18,10 @@ class SessionsController < ApplicationController
 				else 
 					redirect_to root_url
 				end
-			else 
-				flash.now[:alert] = 'Confirm your email my dood'
-				render 'new'
-			end
+			#else 
+				#flash.now[:alert] = 'Confirm your email my dood'
+				#render 'new'
+			#end
 
 
 		else
