3a6d27e6c08a792838a749ec06c3b1c501a64840
geniusrobotizo@gmail.com <geniusrobotizo@gmail.com>
2019-08-23 19:14:08 -0700

    Init Test

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+	default from: "Proximy info@proximy.ca"
+
+	def registration_confirmation(user)
+		@user = user
+		mail(to: @user.email, subject: "Registration Confirmation")
+	end
+
+end
