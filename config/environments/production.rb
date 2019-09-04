aa5f5622d725607c9a6c43f9c0da3a3b369e4fad
geniusrobotizo@gmail.com <geniusrobotizo@gmail.com>
2019-09-04 15:53:16 -0700

    Sep 4 P 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,7 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }
+   config.action_mailer.default_url_options = {host: "www.proximy.ca"}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
