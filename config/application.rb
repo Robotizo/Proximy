6bf3f22482516f78a88ff8062d8ff34296a0e214
geniusrobotizo@gmail.com <geniusrobotizo@gmail.com>
2020-01-29 18:29:55 -0800

    Push 1

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ Bundler.require(*Rails.groups)
 module ContactsApp
   class Application < Rails::Application
   	config.action_view.embed_authenticity_token_in_remote_forms = true
+
+  	
   	config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
