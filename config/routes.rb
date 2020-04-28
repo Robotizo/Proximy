aa2d4cd6b8d015b692d65dc1e3f67f9c81b3b2d5
LuckyGStar <andylee.dreamsoft@outlook.com>
2020-04-27 17:22:42 -0700

    step1 : cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,52 +1,23 @@
  Rails.application.routes.draw do
-
+  root to: 'main#index'
 
   get 'auth/:provider/callback', to: 'sessions#create'
-
-
   get 'auth/failure', to: redirect('/sign_in')
   delete 'signout', to: 'sessions#destroy', as: 'signout'
 
-
-
   resources :after_signup
-
-
-
-
   resources :test_users
-  get 'chats', :to => 'chats#index'
-
   resources :event_comments
   resources :group_comments
   resources :comments
   resources :feedbacks
   resources :friend_requests
-
-
-  resources :friendships do 
-    patch :update_friendship, on: :member
-  end
-
-
-  get '/contacts', to: 'contacts#index'
-
-  get '/notifications', to: 'notifications#index'
-
-
-  resources :notifications do 
-    patch :update_notif, on: :member
-  end 
-
-  
-
   resources :notification_events
   resources :locations
   resources :conversations, only: [:new, :create, :show, :index]
-
-
-
   resources :interests
+  resources :posts
+  resources :users
   resources :event_posts
   resources :group_posts
   resources :groups_relationships, only: [:create, :destroy]
@@ -54,7 +25,16 @@
   resources :interests_relationships, only: [:create, :destroy]
   resources :interests_relationships_to_groups, only: [:create, :destroy]
   resources :interests_relationships_to_events, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  resource :session, controller: 'sessions', only: [:create]
+    
+  resources :friendships do
+    patch :update_friendship, on: :member
+  end
 
+  resources :notifications do
+    patch :update_notif, on: :member
+  end 
 
   resources :events do
     collection do 
@@ -66,13 +46,8 @@
     end 
   end
 
-
-
-
-
-
-  resources :groups do 
-    collection do 
+  resources :groups do
+    collection do
       get :mygroups
     end
 
@@ -81,23 +56,7 @@
     end 
   end
 
-
-  resources :posts
-
-
-
-  get 'main/test'
-
-  get 'main/privacy'
-  get 'main/terms'
-
-  get 'sessions/new'
-  get    '/signup',  to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-
-
+  
   resources :users do
     collection do 
       get :discover
@@ -115,28 +74,25 @@
     end
   end
 
-  resources :relationships, only: [:create, :destroy]
+  # custom auth routes
+  get 'sessions/new'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/sign_in' => 'sessions#new', :as => nil
+  delete '/logout', to: 'sessions#destroy'
 
-    resource :session,
-      controller: 'sessions',
-      only: [:create]
+  get 'main/test'
+  get 'main/privacy'
+  get 'main/terms'
+  get 'chats', :to => 'chats#index'
+  get '/contacts', to: 'contacts#index'
+  get '/notifications', to: 'notifications#index'
 
-
-get '/sign_in' => 'sessions#new', :as => nil
-
-
-
-
-  root to: 'main#index'
-
- 
-
-  resources :users
-
-  mount ActionCable.server => '/cable'
-  
   get 'chats/list_messages/:id', :to => 'chats#list_messages'
   get 'chats/ajax_messages', :to => 'chats#ajax_messages'
+
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
