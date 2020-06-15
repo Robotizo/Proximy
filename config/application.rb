f24086ae134b9e1f6bedf309d7595ba14dea7d06
geniusrobotizo@gmail.com <geniusrobotizo@gmail.com>
2020-06-14 17:12:38 -0700

    FB adds Groups

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module ContactsApp
 
   	
   	config.assets.initialize_on_precompile = false
+  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
